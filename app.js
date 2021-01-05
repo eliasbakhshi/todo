3f9ea408b50d6b48dc8f6b948811bd4164534f54
Elias <bakhshielias@gmail.com>
2021-01-05 20:12:29 +0100

    Version 1.0 is ready to use.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const form = document.querySelector(".enter");
 const searchBox = document.querySelector(".search");
 let list = document.querySelector(".list");
 let table = document.querySelector(".table");
+let tabs = document.querySelector(".tabs");
+let noRes = document.getElementById("searchResult");
 let todos = Array.from(list.children);
 const todoBtn = document.getElementById("todoBtn");
 const doneBtn = document.getElementById("doneBtn");
-const tabs = document.tabs.tabBtn;
 let printedList = [], idCount = 1;
 
 /* Show the relevant list*/
@@ -29,30 +30,18 @@ function showList(listName) {
                     todo.prepend(label);
                     todo.prepend(checkbox);
                 }
-
-
-
-
                 let theCheckbox = todo.getElementsByTagName("INPUT")[0];
                 let theLabel = todo.getElementsByTagName("LABEL")[0];
-                console.log(todo.getElementsByTagName("INPUT"))
-                // console.log(todo)
                 /* Add listener for the checkbox */
                 theCheckbox.addEventListener("change", function() {
-                    console.log(todo.getElementsByTagName("INPUT"))
                     if (this.checked) {
-                        todo.classList.add("done");
-                        todo.classList.add("d-hide");
+                        todo.classList.add("done", "d-hide");
                         setTimeout(() => todo.classList.add("d-none"), 950);
                         todo.classList.remove("todo");
                     } else {
                         todo.classList.remove("done");
                     }
                 });
-
-                console.log(idCount)
-
-
                 /* Match the checkbox with the label */
                 theCheckbox.id = `c${idCount}`;
                 theCheckbox.name = `c${idCount}`;
@@ -70,58 +59,61 @@ function showList(listName) {
                     let iElement = document.createElement("i");
                     iElement.classList.add("fas", "fa-undo-alt", "fa-lg", "undo");
                     iElement.addEventListener("click", function(e) {
-                        todo.classList.add("todo");
-                        todo.classList.add("reset");
+                        todo.classList.add("todo", "reset");
                         setTimeout(() => todo.classList.add("d-none"), 450);
                         todo.classList.remove("done");
                     });
                     todo.prepend(iElement);
                 }
             }
-            todo.classList.remove("d-hide");
-            todo.classList.remove("reset");
-            todo.classList.remove("d-none");
+            todo.classList.remove("d-hide", "reset", "d-none");
             outputList.push(todo);
         });
-    // let deleteBoxes = document.querySelectorAll(".deleteBox");
-    // console.log(deleteBoxes)
-    // deleteBoxes.forEach(todo => {
-    //     console.log("ww")
-    //     todo.addEventListener("change", function() {
-    //         if (this.checked) {
-    //             todo.classList.add("done");
-    //             todo.classList.add("d-hide");
-    //             setTimeout(() => todo.classList.add("d-none"), 950);
-    //             todo.classList.remove("todo");
-    //         } else {
-    //             todo.classList.remove("done");
-    //         }
-    //     });
-    // });
     return outputList;
 }
 
 /* search function */
 function search() {
-    let searchedWord = searchBox.value.trim().toLowerCase();
-    // Disappear todos which are not match.
-    printedList.filter(todo => !todo.textContent.toLowerCase().includes(searchedWord))
-        .forEach(notSearched => notSearched.classList.add("d-none"));
-    // Appear todos again that match.
-    printedList.filter(todo => todo.textContent.toLowerCase().includes(searchedWord))
-        .forEach(Searched => Searched.classList.remove("d-none"));
-}
+    let searchMessage = document.createElement("P");
+    searchMessage.classList.add("no-result");
+    let searchedWord = searchBox.value.trim();
+    if (searchedWord.length) {
+        // Disappear todos which are not match.
+        printedList.filter(todo => !todo.textContent.toLowerCase().includes(searchedWord.toLowerCase()))
+            .forEach(notSearched => notSearched.classList.add("d-none"));
+        // Appear todos again that match.
+        let searchResult = printedList.filter(todo => todo.textContent.toLowerCase().includes(searchedWord.toLowerCase()));
+        if (searchResult.length) {
+            let pluralSearch = (searchResult.length > 1) ? "s" : "";
+            noRes.innerHTML = `Result${pluralSearch} for ${searchedWord}`
+            noRes.classList.add("no-result");
+            searchResult.forEach(Searched => Searched.classList.remove("d-none"));
 
-
-function showForm(show = true) {
-    if (show) {
-        form.classList.remove("hide");
-        table.classList.remove("high");
-    } else {
-        form.classList.add("hide");
-        table.classList.add("high");
+        } else if (!searchResult.length) {
+            noRes.innerHTML = "There is no result to show :(";
+            noRes.classList.add("no-result");
+        }
+    } else if (!searchedWord.length) {
+        noRes.innerHTML = "";
+        noRes.classList.remove("no-result");
     }
 
+
+    }
+
+/* Show the form down to the page */
+function showForm(show = true) {
+    if (show) {
+        form.classList.add("show");
+        form.classList.remove("hide");
+        table.classList.remove("high");
+        setTimeout(() => form.style.display = "flex", 300);
+    } else {
+        form.classList.remove("show");
+        form.classList.add("hide");
+        table.classList.add("high");
+        setTimeout(() => form.style.display = "none", 150);
+    }
 }
 
 
@@ -133,6 +125,7 @@ todoBtn.setAttribute("checked", "checked");
 /* Show the To-do list */
 todoBtn.addEventListener("click", () => {
     printedList = showList("todo");
+    tabs.classList.remove("done");
     search();
     showForm();
 });
@@ -141,6 +134,7 @@ todoBtn.addEventListener("click", () => {
 /* Show the Done list */
 doneBtn.addEventListener("click", () => {
     printedList = showList("done");
+    tabs.classList.add("done");
     search();
     showForm(false);
 });
@@ -151,10 +145,10 @@ form.addEventListener("submit", sub => {
     sub.preventDefault();
     let enteredData = form.data.value.trim();
     if (enteredData.length) {
-        list.innerHTML += `<article class="todo">
+        list.innerHTML = `<article class="todo">
                                 <p>${enteredData}</p>
-                                <i class="fas fa-trash-alt fa-2x trash"></i>
-                           </article>`;
+                                <i class="fas fa-trash-alt fa-lg trash"></i>
+                           </article>` + list.innerHTML;
     }
     form.reset();
     list = document.querySelector(".list");
@@ -170,6 +164,7 @@ list.addEventListener("click", e => {
         e.target.parentElement.classList.add("reset");
         setTimeout(() => e.target.parentElement.remove(), 450);
     }
+    search();
 });
 
 
